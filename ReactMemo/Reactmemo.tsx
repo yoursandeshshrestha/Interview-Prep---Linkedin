c3bd660c69c1303456fd7e2212abe78b238daca0
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-08-28 23:09:13 +0530

    Day-7 ReactMemo Code

---
+++ ReactMemo/Reactmemo.tsx
@@ -0,0 +1,19 @@
+//  ============ Commented out because we haven't initialized the React app yet. ============//
+
+// import React from 'react';
+
+// const MyComponent = React.memo(({ data }) => {
+//   // Component logic and rendering
+//   return <div>{data}</div>;
+// });
+
+// const MyComponent = React.memo(
+//     ({ data }) => {
+//       // Component logic and rendering
+//       return <div>{data}</div>;
+//     },
+//     (prevProps, nextProps) => {
+//       // Custom comparison logic
+//       return prevProps.data === nextProps.data;
+//     }
+//   );
